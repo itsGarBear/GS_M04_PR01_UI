2d2bef3dd1effe70f0ad8592420f776739e5cb0f
itsGarBear <g.smith61900@gmail.com>
2021-05-03 13:10:47 -0700

    Add files via upload

---
+++ Scripts/Type.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Type : MonoBehaviour
+{
+    public TMP_Text text;
+
+    private void Start()
+    {
+        text.text = "";
+    }
+    public void AddText(string character)
+    {
+        text.text += character;
+    }
+    public void SubtractText()
+    {
+        if(text.text.Length != 0)
+            text.text = text.text.Substring(0, text.text.Length - 1);
+    }
+}
